01fbbf552405d242658d447beefe9c373569680b
Saydur Rahman <saydur_93@live.com>
2018-09-04 19:57:15 +0100

    Added new function that will render only html

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
+var faqContent = document.getElementById("container");
 var dataRequest = new XMLHttpRequest();
 dataRequest.open('GET', 'https://design.propcom.co.uk/buildtest/accordion-data.json');
 
 dataRequest.onload = function() {
-    // console.log(dataRequest.responseText);
 
     var faqData = JSON.parse(dataRequest.responseText);
+    renderHTML(faqData.blocks);
 
-    console.log(faqData.blocks[0].heading);
+    // console.log(faqData.blocks[0].heading);
+
+
 };
 dataRequest.send();
+
+
+function renderHTML(data) {
+
+    for(i = 0; i < data.length; i++){
+        document.write('<button>'+data[i].heading+'</button>');
+        console.log(data[i].heading);
+    }
+
+}
